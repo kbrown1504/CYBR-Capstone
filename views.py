132ab773cbff9a90524fa9a43b5f9b78c2a5d3a5
kbrown1504 <keegqbrown@gmail.com>
2022-04-23 01:13:38 -0500

    fixing table html

--- views.py
+++ views.py
@@ -345,8 +345,8 @@ def details(request, address):
 
 				issueCount += 1
 
-				issueDesc = json.dumps(issue['description'].strip('\"'))
-				issueId = int(json.dumps(issue['@osvdbid'].strip('\"')))
+				issueDesc = json.dumps(issue['description']).strip('\"')
+				issueId = int(json.dumps(issue['@osvdbid']).strip('\"'))
 				issueLink = json.dumps(issue['@osvdblink']).strip('\"')
 
 				r['niktotable'] += '<div class="small" style="margin-top:10px;">'
