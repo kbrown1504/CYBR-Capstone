75fb12fc153fa00442cf4473c348fac8a8c24ed6
kbrown1504 <keegqbrown@gmail.com>
2022-04-24 15:23:18 -0500

    fixing scan bug

--- views.py
+++ views.py
@@ -333,7 +333,7 @@ def details(request, address):
 	niktoScans = os.listdir(niktoDir)
 	for scan in niktoScans:
 		d = xmltodict.parse(open(niktoDir+scan, 'r').read())
-		niktoAddr = json.dumps(d.get('niktoscan').get('niktoscan')[0].get('scandetails').get('@targetip').strip('\"')
+		niktoAddr = json.dumps(d.get('niktoscan').get('niktoscan')[0].get('scandetails').get('@targetip')).strip('\"')
 		if niktoAddr == r['address']:
 			# niktoJson = json.dumps(d['niktoscan']).strip('\"')
 			r['niktocommand'] = '<p>nikto ' + json.dumps(d['niktoscan']['niktoscan'][0]['@options']).strip('\"') + '</p>'
